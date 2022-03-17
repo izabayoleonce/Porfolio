b1b7dbdf31d5fcc2c075b9396fac00937766dfa5
izabayoleonce <izabayoleonce@gmail.com>
2022-03-17 09:31:48 +0000

    first commit

---
+++ acceuil/entrprise.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/entreprise.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>cd-media</title>
+</head>
+<body>
+<nav class="navbar navbar-light bg-light">
+  <h4>Leonce IZABAYO</h4>
+  <div class="container">
+    <ul class="nav justify-content-end">
+    <li class="nav-item">
+        <a class="nav-link" href="./index.php">Acceuil</a>
+      </li>
+      <li class="nav-item dropdown">
+            <a  class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">Mon Profile</a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="./a_propos.php">Bio</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./entrprise.php">Mon Entreprise</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link " href="./Projet.php" role="button" aria-expanded="true">projet</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="./veille.php" tabindex="-1" aria-disabled="true">Ma veille</a>
+      </li>
+    </ul>
+
+  </div>
+  <a  type="button" href="./document/LEONCE_IZABAYO_CV_ .pdf" class="btn btn-outline-success">Mon Cv</a>
+</nav>
+<main class="">
+  <div class="job">
+    <section class="corpEntreprise">
+      <div class="header">
+        <div class="row">
+          <div class="col-sm-7 col-md-8">
+            <div class="header-main-infos">
+              <div class="nom">Cd Média</div>
+              <div class="niche"><span>Internet:</span><span>création des sites,hébergement</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="navcd">
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+
+      <div class="main">
+        <div class="left">
+          <div class="teaser">
+            <div class="coordonne">
+              <p> <span>0678217876</span><i class="bi bi-telephone"></i></p>
+            </div>
+          </div>
+        </div>
+        <div class="rigth">
+          <div class="maps"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2884.293029086484!2d7.264378515343635!3d43.70445907911971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12cddaa81960cafb%3A0xea2f207dec1ed4a9!2s31%20Rue%20de%20Paris%2C%2006000%20Nice!5e0!3m2!1sfr!2sfr!4v1645006731382!5m2!1sfr!2sfr" width="600" height="450" right="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
+        </div>
+      </div>
+
+    </section>
+  </div>
+
+</main>
+<footer>
+  <a href="tel:+330676770328"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+</svg></a>
+  <a href="https://twitter.com/izabayoleonce"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+</svg></a>
+<a href="http://www.linkedin.com/in/leonce-izabayo/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+</svg></a>
+<a href="mailto:Leonce.Izabayo@outlook.fr"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+</svg></a>
+
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
+</body>
+</html>
