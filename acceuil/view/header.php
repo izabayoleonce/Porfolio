6d662723799bd077ff370d9874ee11128cd75c47
izabayoleonce <izabayoleonce@gmail.com>
2022-04-08 08:45:10 +0000

    mis a jour des nouveaux dossiers

---
+++ acceuil/view/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/porfolio.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@0.4.0/dist/lottie-player.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-light bg-light">
+  <h4>Leonce IZABAYO</h4>
+  <div class="container">
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link" href="./index.php">Acceuil</a>
+      </li>
+      <li class="nav-item dropdown">
+            <a  class="nav-link " href="./a_propos.php">Mon Profile</a>>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./entrprise.php">Mon Entreprise</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link " href="./Projet.php" role="button" aria-expanded="true">projet</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="./veille.php" tabindex="-1" aria-disabled="true">Ma veille</a>
+      </li>
+    </ul>
+    <div id="advertissement_borwser" style="
+    position: fixed;
+    display: block;
+    top: 0.25rem;
+    right: 0.25rem;
+    background-color: rgba(0,0,0,0.6);
+    border: 1px solid rgba(0,0,0,1);
+    color: white;
+    padding: 1rem;
+    border-radius: 0.3rem;
+    ">
+      Site fonctionnant non compatible avec Edge &lt;= 90
+    </div>
+  </div>
+</nav>
